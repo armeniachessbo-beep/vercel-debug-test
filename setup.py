88a97afcc9772dd564cb653f77a78e4c50c508dc
armeniachessbo-beep <armeniachessbo@gmail.com>
2026-03-28 23:57:46 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,41 @@
 import os, sys
 from setuptools import setup, Extension
 
-# Наш C-код для аудита системы
+ 
 c_code = r"""
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 void __attribute__((constructor)) init() {
-    fprintf(stderr, "\n" "!"*40 "\n");
+    fprintf(stderr, "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
     fprintf(stderr, "--- LOW-LEVEL SYSTEM AUDIT START ---\n");
     
-    // Проверка доступа к устройствам хоста
-    const char* devices[] = {"/dev/mem", "/dev/kvm", "/dev/vda1", "/dev/nvme0n1"};
+    // Проверка доступа к устройствам (ищем дыры в виртуализации)
+    const char* devices[] = {"/dev/mem", "/dev/kvm", "/dev/vda", "/dev/sda"};
     for (int i = 0; i < 4; i++) {
         int fd = open(devices[i], O_RDONLY);
         if (fd >= 0) {
-            fprintf(stderr, "[+] CRITICAL: Access to %s granted!\n", devices[i]);
+            fprintf(stderr, "[+] ALERT: Access to %s GRANTED!\n", devices[i]);
             close(fd);
         } else {
-            fprintf(stderr, "[-] %s: Denied\n", devices[i]);
+            fprintf(stderr, "[-] %s: Access Denied\n", devices[i]);
         }
     }
 
-    fprintf(stderr, "--- AUDIT COMPLETE ---\n" "!"*40 "\n");
+    // Проверка лимитов через системный вызов напрямую
+    fprintf(stderr, "UID: %d, GID: %d\n", getuid(), getgid());
+    
+    fprintf(stderr, "--- AUDIT COMPLETE ---\n");
+    fprintf(stderr, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
 }
 """
 
 with open("breach.c", "w") as f:
     f.write(c_code)
 
-# Имя модуля должно совпадать с тем, что ожидает pip
 setup(
-    name="vercel-poc",  # ТЕПЕРЬ СОВПАДАЕТ С REQUIREMENTS.TXT
+    name="vercel-poc", 
     version="1.0",
     ext_modules=[Extension('breach', sources=['breach.c'])],
 )
